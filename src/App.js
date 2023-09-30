571d3c28e5d208ffef8534ab836f52770dab0c1d
Jeremy Lee <z5316983@ad.unsw.edu.au>
2023-09-30 20:58:26 +1000

    shit navbar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import './App.css';
 // additional imports
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+// file imports
+import Navbar from './components/Navbar'
+import Ryan from './pages/Ryan'
+import Facts from './pages/Facts'
+import Background from './pages/Background'
+import Hello from './pages/Hello'
+
 function App() {
   return (
-    <BrowserRouter basename={config.BASE_NAME}>
+    <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/ryan' element={<Ryan />}/>
+        <Route path='/' element={<Ryan />}/>
         <Route path='/facts' element={<Facts />}/>
         <Route path='/background' element={<Background />}/>
+        <Route path='/hello' element={<Hello />}/>
       </Routes>
     </BrowserRouter>
   );
