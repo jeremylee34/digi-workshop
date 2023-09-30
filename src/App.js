0c8be91fb5ad1ad8e75fd70e0f2e0f839876a664
Jeremy Lee <z5316983@ad.unsw.edu.au>
2023-09-30 20:46:25 +1000

    added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+// additional imports
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={config.BASE_NAME}>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/ryan' element={<Ryan />}/>
+        <Route path='/facts' element={<Facts />}/>
+        <Route path='/background' element={<Background />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
